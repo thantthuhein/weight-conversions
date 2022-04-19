0a627ec022735979ce22345734c07d2b0189f03c
thantthuhein <thantthuhein@geniefintech.com>
2022-04-19 14:25:06 +0630

    Feat: add toPounds

--- src/Weight.php
+++ src/Weight.php
@@ -4,4 +4,20 @@ namespace  ThantDev\WeightConversions;
 
 class Weight
 {
+     protected $kilograms;
+
+     public static function kilograms(float $kilograms): self
+     {
+          return new static($kilograms);
+     }
+
+     public function __construct($kilograms)
+     {
+          $this->kilograms = $kilograms;
+     }
+
+     public function toPounds(): float
+     {
+          return $this->kilograms * 2.2046;
+     }
 }
---
+++ tests/WeightTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use ThantDev\WeightConversions\Weight;
+
+it('can convert kilograms to pounds', function () {
+    $pounds = Weight::kilograms(100)->toPounds();
+
+    expect($pounds)->toEqual(220.46);
+});
