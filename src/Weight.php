bbf43238cd6583db56f7b8b3dda71356cf7fe043
thantthuhein <thantthuhein@users.noreply.github.com>
2022-04-19 07:55:29 +0000

    Fix styling

--- src/Weight.php
+++ src/Weight.php
@@ -4,20 +4,20 @@ namespace  ThantDev\WeightConversions;
 
 class Weight
 {
-     protected $kilograms;
+    protected $kilograms;
 
-     public static function kilograms(float $kilograms): self
-     {
-          return new static($kilograms);
-     }
+    public static function kilograms(float $kilograms): self
+    {
+        return new static($kilograms);
+    }
 
-     public function __construct($kilograms)
-     {
-          $this->kilograms = $kilograms;
-     }
+    public function __construct($kilograms)
+    {
+        $this->kilograms = $kilograms;
+    }
 
-     public function toPounds(): float
-     {
-          return $this->kilograms * 2.2046;
-     }
+    public function toPounds(): float
+    {
+        return $this->kilograms * 2.2046;
+    }
 }
